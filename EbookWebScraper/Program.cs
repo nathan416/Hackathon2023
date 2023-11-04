f3152394c7eaf608d69cf56751a623dfeb22ba63
nathan416 <nathan.flack@gmail.com>
2023-11-04 10:38:17 -0700

    initial commit

---
+++ EbookWebScraper/Program.cs
@@ -0,0 +1,8 @@
+﻿// GU Hackathon 2023
+// Project: EbookWebScraper
+// Author: Nathan Flack
+// Team Name: The Llammas
+// Date: 11/4/2023
+Console.WriteLine("Please enter URL for index page");
+string? IndexURL = Console.ReadLine();
+Console.WriteLine(IndexURL);

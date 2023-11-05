eef74bc1ffb44350529fd25a43dc34c93e342901
nathan416 <nathan.flack@gmail.com>
2023-11-04 20:39:22 -0700

    Changed from Aspose to vieapps. mvp completed mostly

--- EbookWebScraper/Program.cs
+++ EbookWebScraper/Program.cs
@@ -5,19 +5,24 @@
 // Date: 11/4/2023
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using Aspose.Words;
+using Aspose.Words.Saving;
+using System.Text;
+using net.vieapps.Components.Utility.Epub;
+using ShellProgressBar;
 
 string Pattern = @"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
 
 
-EbookWebScraper scraper = new();
+Ebook scraper = new();
 bool IsValidURL = false;
 string? IndexURL;
 do
 {
     Console.WriteLine("Please enter URL for index page");
     IndexURL = Console.ReadLine();
-    //IndexURL = "https://www.royalroad.com/fiction/75025/clover-a-litrpg-apocalypse?source=recommender&recommender_version=5";
-    IndexURL = "https://www.royalroad.com/fiction/61480/amelia-the-level-zero-hero-an-op-mc-isekai-litrpg";
+    IndexURL = "https://www.royalroad.com/fiction/75025/clover-a-litrpg-apocalypse?source=recommender&recommender_version=5";
+    //IndexURL = "https://www.royalroad.com/fiction/61480/amelia-the-level-zero-hero-an-op-mc-isekai-litrpg";
     Match URLMatch = Regex.Match(IndexURL, Pattern);
     if (URLMatch.Success)
     {
@@ -30,35 +35,131 @@ do
 } while (!IsValidURL);
 if (IsValidURL)
 {
+    //Console.ReadKey();
+
+    //Console.WriteLine(scraper.Title);
+    //Console.WriteLine(scraper.Author);
+    //foreach (var chapter in scraper.ChapterList)
+    //{
+    //    Console.WriteLine(chapter.ChapterTitle);
+    //    Console.WriteLine(chapter.ChapterURL);
+    //}
+    //Console.WriteLine(scraper.ChapterList.Count);
+    //scraper.ChapterList[63].GetChapterWebData();
+    //Console.WriteLine(scraper.ChapterList[63].ChapterBody[0].InnerText);
     scraper.GetIndexWebData(IndexURL);
-    Console.ReadKey();
+    scraper.GetAllChapterText();
+    scraper.SaveToEpub();
 
-    Console.WriteLine(scraper.Title);
-    Console.WriteLine(scraper.Author);
-    foreach (var chapter in scraper.ChapterList)
-    {
-        Console.WriteLine(chapter.ChapterTitle);
-        Console.WriteLine(chapter.ChapterURL);
-    }
-    Console.WriteLine(scraper.ChapterList.Count);
-    scraper.ChapterList[63].GetChapterWebData();
-    Console.WriteLine(scraper.ChapterList[63].ChapterBody[0].InnerText);
+    //Console.WriteLine("{0}", URLMatch.Value);
 }
-
-//Console.WriteLine("{0}", URLMatch.Value);
-class EbookWebScraper
+class Ebook
 {
     private readonly string BASEURL = "https://www.royalroad.com";
     public int ChapterAmount { get; set; }
     public string? Author { get; set; }
     public string? Title { get; set; }
     public List<Chapter> ChapterList { get; set; }
-    public EbookWebScraper()
+    private net.vieapps.Components.Utility.Epub.Document Epub = new();
+ 
+    public Ebook()
     {
         ChapterList = new();
     }
+    public string CoverImage { get; set; }
+    public net.vieapps.Components.Utility.Epub.Document Epub1 { get => Epub; set => Epub = value; }
 
-    public string Epub = "";
+    public void GetAllChapterText()
+    {
+        var options = new ProgressBarOptions
+        {
+            ProgressCharacter = '─',
+            ProgressBarOnBottom = true
+        };
+        using (var pbar = new ProgressBar(ChapterList.Count, "Getting Chapters from the internet", options))
+        {
+            foreach (Chapter chapter in ChapterList)
+            {
+                chapter.GetChapterWebData();
+                pbar.Tick();
+                Thread.Sleep(200);
+            }
+        }
+    }
+    public void SaveToEpub()
+    {
+        //HtmlSaveOptions saveOptions = new HtmlSaveOptions();
+        //saveOptions.SaveFormat = SaveFormat.Epub;
+        //saveOptions.Encoding = Encoding.UTF8;
+        //// create a blank document
+        //Document doc = new Document();
+        //// the DocumentBuilder class provides members to easily add content to a document
+        //DocumentBuilder builder = new DocumentBuilder(doc);
+        //// write a new paragraph in the document
+        //foreach (Chapter chapter in ChapterList)
+        //{
+        //    //builder.Writeln(chapter.ChapterTitle);
+        //    builder.InsertHtml(string.Format("<h1>{0}</h1>", chapter.ChapterTitle));
+        //    foreach (HtmlNode paragraph in chapter.ChapterBody)
+        //    {
+        //        builder.Writeln(paragraph.InnerText);
+        //        builder.Writeln();
+        //        builder.Writeln();
+        //    }
+        //}
+        //saveOptions.DocumentSplitCriteria = DocumentSplitCriteria.HeadingParagraph;
+
+        //// Specify that we want to export document properties.
+        //saveOptions.ExportDocumentProperties = true;
+
+        //doc.Save("C:\\Users\\natha\\OneDrive - Gonzaga University\\Desktop\\Hackathon2023\\EbookWebScraper\\output\\" + "output.epub", saveOptions);
+        var Uuid = Guid.NewGuid();
+        Epub1.AddBookIdentifier(Uuid.ToString());
+        Epub1.AddLanguage("English");
+        Epub1.AddTitle(Title);
+        Epub1.AddAuthor(Author);
+        string readText = File.ReadAllText("C:\\Users\\natha\\OneDrive - Gonzaga University\\Desktop\\Hackathon2023\\EbookWebScraper\\stylesheet.css");
+        Epub1.AddStylesheetData("style.css", readText);
+
+        //var coverImageId = Epub.AddImageData("cover.jpg", coverImageBinaryData);
+        //Epub.AddMetaItem("cover", coverImageId);
+
+        var pageTemplate = @"<!DOCTYPE html>
+	    <html xmlns=""http://www.w3.org/1999/xhtml"">
+		    <head>
+			    <title>{0}</title>
+			    <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8""/>
+			    <link type=""text/css"" rel=""stylesheet"" href=""style.css""/>
+			    <style type=""text/css"">
+				    @page {
+					    padding: 0;
+					    margin: 0;
+				    }
+			    </style>
+		    </head>
+		    <body>
+			    {1}
+		    </body>
+	    </html>".Trim().Replace("\t", "");
+
+        // cover
+        Epub1.AddXhtmlData("page0.xhtml", pageTemplate.Replace("{0}", Title).Replace("{1}", "Hello World"));
+
+        // chapter
+        for (var index = 0; index < ChapterList.Count; index++)
+        {
+            var name = string.Format("page{0}.xhtml", index + 1);
+            var content = ChapterList[index];
+
+            
+
+            Epub1.AddXhtmlData(name, pageTemplate.Replace("{0}", content.ChapterTitle).Replace("{1}", content.ChapterBody.InnerHtml));
+            Epub1.AddNavPoint(content.ChapterTitle + " - " + (index + 1).ToString(), name, index + 1);
+        }
+
+        Epub1.Generate("C:\\Users\\natha\\OneDrive - Gonzaga University\\Desktop\\Hackathon2023\\EbookWebScraper\\output\\output.epub");
+
+    }
 
     public void GetIndexWebData(string URL)
     {
@@ -83,8 +184,6 @@ class EbookWebScraper
         //.First()
         //.OuterHtml;
     }
-
-    public void AddChapterToEpub
 }
 
 class Chapter
@@ -97,7 +196,7 @@ class Chapter
 
     public string? ChapterTitle { get; set; }
     public string? ChapterURL { get; set; }
-    public HtmlNodeCollection? ChapterBody { get; set; }
+    public HtmlNode? ChapterBody { get; set; }
     public HtmlNodeCollection? ChapterPreAuthorNote { get; set; }
     public HtmlNodeCollection? ChapterPostAuthorNote { get; set; }
 
@@ -106,8 +205,8 @@ class Chapter
         HtmlWeb web = new();
         HtmlDocument doc = web.Load(ChapterURL);
 
-        HtmlNodeCollection bodyNode = doc.DocumentNode.SelectNodes("//*[@class=\"chapter-inner chapter-content\"]/p");
-        ChapterBody = bodyNode[0].ChildNodes;
+        HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//*[@class=\"chapter-inner chapter-content\"]");
+        ChapterBody = bodyNode;
         
 
         //TODO: Not working right now
